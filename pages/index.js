a0c891a58b3c98ab82e38c4a1397695c6828463c
anouar garbaa <g.anouar@yahoo.com>
2024-11-21 14:02:06 +0100

    adding Layout wrapper

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,25 @@ import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
   {
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    address: 'Habib Bourguiba avenue, 4081 Sousse',
-    description: 'This is a first meetup!'
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    address: "Habib Bourguiba avenue, 4081 Sousse",
+    description: "This is a first meetup!",
   },
   {
-    id: 'm2',
-    title: 'A second Meetup',
-    image: 'https://images.pexels.com/photos/2581922/pexels-photo-2581922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    address: 'Freedom avenue, 4000 Tunis',
-    description: 'This is a second meetup!'
-  }
-]
+    id: "m2",
+    title: "A second Meetup",
+    image:
+      "https://images.pexels.com/photos/2581922/pexels-photo-2581922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    address: "Freedom avenue, 4000 Tunis",
+    description: "This is a second meetup!",
+  },
+];
 
 function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
