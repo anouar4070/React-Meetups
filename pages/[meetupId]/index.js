b038f1cfcd02550bdccc51f213cfa2458dec5b95
anouar garbaa <g.anouar@yahoo.com>
2024-11-21 14:41:43 +0100

    adding cstom MeetupDetail component

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      title="A First Meetup"
+      image="https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      address="Habib Bourguiba avenue, 4081 Sousse"
+      description="This is a first meetup!"
+    />
+  );
+}
+
+export default MeetupDetails;
