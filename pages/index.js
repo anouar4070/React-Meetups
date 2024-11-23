c2247b86ee41c5931ece58ee1f0719dc145bf472
anouar garbaa <g.anouar@yahoo.com>
2024-11-23 13:12:36 +0100

    adding sever-side Rendering

--- pages/index.js
+++ pages/index.js
@@ -21,35 +21,55 @@ const DUMMY_MEETUPS = [
 ];
 
 function HomePage(props) {
-//   const [loadedMeetups, setLoadedMeetups] = useState([]);
-// useEffect(() => {
-//  //send http req & fetch data
-//   setLoadedMeetups(DUMMY_MEETUPS);
-// }, [])
-// return <MeetupList meetups={loadedMeetups} />;
+  //   const [loadedMeetups, setLoadedMeetups] = useState([]);
+  // useEffect(() => {
+  //  //send http req & fetch data
+  //   setLoadedMeetups(DUMMY_MEETUPS);
+  // }, [])
+  // return <MeetupList meetups={loadedMeetups} />;
 
   return <MeetupList meetups={props.meetups} />;
-
 }
 
-//* ** Data fetching for static Pages ** 
-export async function getStaticProps() {
-//fetch data from an API
-return {
-  props: {
-    meetups: DUMMY_MEETUPS
-  }
+//* ** Server-side Rendering **
+export async function getServerSideProps(context) {
+  const req = context.req;
+  const res = context.res;
+
+  //fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
 }
-}
+
+
+
+//* ** Data fetching for static Pages **
+// export async function getStaticProps() {
+// //fetch data from an API
+// return {
+//   props: {
+//     meetups: DUMMY_MEETUPS
+//   },
+//   revalidate: 10
+// }
+// }
 
 export default HomePage;
 
 
-//* ** Data fetching for static Pages ** 
+
+
+
+
+
+//* ** Data fetching for static Pages **
 //getStaticProps() prepare props for the HomePage
 //this code is executing during the build process not on the server & not on the client
 //with that our DUMMY_MEETUPS will be loaded and prepared in getStaticProps and the they wil be set as props for HomePage component
 //we move the data fetching away from the client to the server-side (to the during the built process side)
 //if we view the page source (inspect element) we see that we no longer have an empty unordered list (the case that we saw using useEffect())
 //now this is pre-rendered, and it now contains the full HTML code (it's great for search engine)
-//data is not fetched in  a second component render cycle on the client, but initially. 
+//data is not fetched in  a second component render cycle on the client, but initially.
