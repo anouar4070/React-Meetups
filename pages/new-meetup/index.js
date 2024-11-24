a8aed1abd1e724fa396ada314ee9729b4d8e15ad
anouar garbaa <g.anouar@yahoo.com>
2024-11-24 13:19:39 +0100

    sending http req to the API route

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,14 +1,30 @@
 //our-domain.com/new-meetup
 
+import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-function addMeetupHandler(enteredMeetupData) {
-console.log(enteredMeetupData);
-}
+  const router = useRouter();
 
-  
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    //to navigate away
+    router.push("/");
+    //replace() method to make sure tha we can't go back with the back button
+    //router.replace()
+  }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
 
 export default NewMeetupPage;
