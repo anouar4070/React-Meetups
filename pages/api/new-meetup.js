278a76a6f2ab5d315d2824b89cc323d3d3a8f7d3
anouar garbaa <g.anouar@yahoo.com>
2024-11-24 12:55:26 +0100

    adding API Route & connect it to MongoDB Atlas Clustor

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,27 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    //const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://ahmed:ahmedanouar@cluster0.yixb9.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close(); //to close db connection once we're done.
+
+    //res.status(201); //to indicate tha something was inserted successfully.
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
