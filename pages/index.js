a7c8276c1ab48fed6285542f47c099b6a4603a9f
anouar garbaa <g.anouar@yahoo.com>
2025-11-15 22:38:35 +0100

    add comments

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,8 @@ function HomePage(props) {
   );
 }
 
-//* ** Server-side Rendering **
+//* ** Server-side Rendering with "getServerSideProps" **
+/// this func runs for every incoming requests anyways
 // export async function getServerSideProps(context) {
 //   const req = context.req;
 //   const res = context.res;
@@ -60,9 +61,17 @@ function HomePage(props) {
 // }
 
 //* ** Data fetching for static Pages **
+/// This only works in your Page component files
+///we take advantage of the caching & we're not regenerating the page multiple times unnecessarily
 export async function getStaticProps() {
   //fetch data from an API
-  //fetch('/api/meetups');// we don't need it because the code in getStaticProps() will only run on the server.
+  //   return {
+  //     props: {
+  //       meetups: DUMMY_MEETUPS,
+  //     },
+  //     revalidate: 10 ///the page will be regenerated on the server (after deployment) at least every 10s if there's a request coming in for this page
+  //   };
+  //fetch('/api/meetups');/// we don't need it because the code in getStaticProps() will only run on the server.
 
   const client = await MongoClient.connect(
     "mongodb+srv://ahmed:ahmedanouar@cluster0.yixb9.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0"
@@ -93,7 +102,7 @@ export default HomePage;
 //* ** Data fetching for static Pages **
 //getStaticProps() prepare props for the HomePage
 //this code is executing during the build process not on the server & not on the client
-//with that our DUMMY_MEETUPS will be loaded and prepared in getStaticProps and the they wil be set as props for HomePage component
+//with that our DUMMY_MEETUPS will be loaded and prepared in getStaticProps and they will be set as props for HomePage component
 //we move the data fetching away from the client to the server-side (to the during the built process side)
 //if we view the page source (inspect element) we see that we no longer have an empty unordered list (the case that we saw using useEffect())
 //now this is pre-rendered, and it now contains the full HTML code (it's great for search engine)
